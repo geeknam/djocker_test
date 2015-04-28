1cc287a390d79eb6bf79b64a9970e1f6eb91a978
Nam Ngo <emoinrp@gmail.com>
2015-04-29 01:31:29 +1000

    update tests

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-import flaskr
+from app import app
 import unittest
 
 class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.app = flaskr.app.test_client()
+        self.app = app.test_client()
 
     def test_homepage(self):
         rv = self.app.get('/')
         assert 'Hello' in rv.data
+
+if __name__ == '__main__':
+    unittest.main()
